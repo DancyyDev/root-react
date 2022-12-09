3c193429946574d4630dd45ff1b6771afd7428c3
Daniel Cha <cha.danieldev@gmail.com>
2022-12-08 21:12:16 -0500

    first commit

---
+++ src/components/Players.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function players() {
+  return (
+    <div>
+        <h2>Players</h2>
+        <div>
+            <ul>
+                <li>Thief</li>
+                <li>Adventurer</li>
+                <li>Scoundrel</li>
+                <li>Ronin</li>
+                <li>Harrier</li>
+                <li>Ranger</li>
+                <li>Arbiter</li> 
+                <li>Tinker</li> 
+                <li>Vagrant</li> 
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default players
